a80b11c8adf4562d0b011d3b5e350a4aeef71652
kraiwut <bom@oozou.com>
2012-12-18 11:57:20 +0700

    init necklace project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Necklace::Application.config.secret_token = 'a37307b492f6d265c1b1493c13cf2979d3a7320bcf056a616fca009b94bd79626ca29e0394362e9c8e7650386a9780f66b795fe0c9c226c4be407be30b9ad5ac'
