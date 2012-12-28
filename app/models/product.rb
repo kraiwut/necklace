7f8a0adea0681808167e5eb99a1d5382811f66b3
kraiwut <bom@oozou.com>
2012-12-28 13:45:48 +0700

    testing products controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price, :short_description, :seo_link, :images_attributes
 
+  belongs_to :category
   has_many :images
   accepts_nested_attributes_for :images
 end
--- spec/controllers/admin/products_controller_spec.rb
+++ spec/controllers/admin/products_controller_spec.rb
@@ -1,25 +1,50 @@
 require 'spec_helper'
 
 describe Admin::ProductsController  do
+	let(:admin) 					{ User.make! :admin }
+	let!(:home_category)	{ init_home_category }
+
+	before :each do
+		sign_in admin
+	end
 
 	describe "index" do
+		before :each do
+			init_home_category
+		end
 	
 		it "should not allow normal user" do
+			sign_out admin
 			sign_in User.make!
 			get "index", :category_id => Category.make!
 			response.should redirect_to root_path
 		end
 
 		it "should allow admin" do
-			sign_in User.make!(:admin)
 			get "index", :category_id => Category.make!
 			response.should render_template "index"
 		end
 
 		it "should retrieve all product in the category" do
-			sign_in User.make!(:admin)
-			get "index", :category_id => init_categories[1]
+			get "index", :category_id => home_category.children[1]
 			assigns[:products].length.should == 5
+		end
+
+		describe "admin root path" do
+			it "should go to 'Home' category screen" do
+				get "index"
+				assigns[:category].name.should == "Home"
+				assigns[:category].parent_id.should be_nil
+			end
+		end
+
+	end
+
+	describe "#new product" do
+
+		it "should belongs to the category" do
+			get 'new', :category_id => home_category
+			assigns[:product].category.should == home_category
 		end
 
 	end
